fee19009ed1406e9cd5eb0aae6dad6bee1533828
Aza Asim <azaasim@gmail.com>
2025-07-18 18:26:08 +0300

    Setup Solution and Web Project

--- Docs.Web/Program.cs
+++ Docs.Web/Program.cs
@@ -1,11 +1,51 @@
 using Docs.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ShiftSoftware.ShiftBlazor.Extensions;
+using ShiftSoftware.ShiftBlazor.Services;
+using ShiftSoftware.ShiftEntity.Core.Extensions;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseUrl = builder.Configuration!.GetValue<string>("BaseURL")!;
 
-await builder.Build().RunAsync();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
+
+var shiftIdentityApiURL = builder.Configuration!.GetValue<string>("ShiftIdentityApi")!;
+shiftIdentityApiURL = string.IsNullOrWhiteSpace(shiftIdentityApiURL) ? baseUrl : shiftIdentityApiURL; //Fallback to BaseURL if emtpy
+
+var shiftIdentityFrontEndURL = builder.Configuration!.GetValue<string>("ShiftIdentityFrontEnd")!;
+shiftIdentityFrontEndURL = string.IsNullOrWhiteSpace(shiftIdentityFrontEndURL) ? baseUrl : shiftIdentityFrontEndURL; //Fallback to BaseURL if emtpy
+
+builder.Services.AddShiftBlazor(config =>
+{
+    config.ShiftConfiguration = options =>
+    {
+        options.BaseAddress = baseUrl!;
+        options.ExternalAddresses = new Dictionary<string, string?>
+        {
+            ["ShiftIdentityApi"] = shiftIdentityApiURL,
+            ["Docs"] = baseUrl
+        };
+        options.UserListEndpoint = shiftIdentityApiURL.AddUrlPath("IdentityPublicUser");
+
+        options.AddLanguage("en-US", "English")
+               .AddLanguage("ar-IQ", "Arabic", true)
+               .AddLanguage("en-US", "English RTL", true)
+               .AddLanguage("ku-IQ", "Kurdish", true);
+    };
+});
+
+var host = builder.Build();
+
+var setMan = host.Services.GetRequiredService<SettingManager>();
+
+var culture = setMan.GetCulture();
+
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+await host.RunAsync();
