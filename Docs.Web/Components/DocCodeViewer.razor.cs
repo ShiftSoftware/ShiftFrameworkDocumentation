8d377dba53f910395cd8f232b83288cab716ac23
Kodo Yousif <kodo.yousif@gmail.com>
2025-08-09 12:57:49 +0300

    get code file from snippets folder

--- Docs.Web/Components/DocCodeViewer.razor.cs
+++ Docs.Web/Components/DocCodeViewer.razor.cs
@@ -1,25 +1,44 @@
 ﻿using Docs.Web.Interfaces;
 using Microsoft.AspNetCore.Components;
+using static System.Net.WebRequestMethods;
 
 namespace Docs.Web.Components
 {
     public partial class DocCodeViewer
     {
+        [Inject] private NavigationManager Nav { get; set; } = default!;
+        [Inject] private HttpClient Http { get; set; } = default!;
         [Parameter] public required Type DocComponent { get; set; }
         private bool ShowCode;
         private IDocumentedComponent? instance;
+
+        private string? snippet;
 
         private void ToggleCode()
         {
             ShowCode = !ShowCode;
         }
 
+        private async void getTextCode(string fileName)
+        {
+            var url = new Uri(Nav.BaseUri + "snippets/ShiftAutoCompleteBasicUse.txt");
+            snippet = await Http.GetStringAsync(url);
+
+            //Console.WriteLine(snippet);
+        } 
+
         protected override void OnInitialized()
         {
             if (DocComponent != null)
             {
                 instance = Activator.CreateInstance(DocComponent) as IDocumentedComponent;
+
+                var type = DocComponent;
+                var fileName = type.Name + ".razor";
+
+                getTextCode(fileName);
             }
         }
     }
 }
+ 
